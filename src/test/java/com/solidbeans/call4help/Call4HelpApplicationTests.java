2bebbe326f371785ebc879e6762567fe02851592
Andreas Pettersson <andreas.pettersson@solidbeans.com>
2021-01-25 12:01:29 +0100

    Update to Java 15 & Spring Boot 2.4.2

--- src/test/java/com/solidbeans/call4help/Call4HelpApplicationTests.java
+++ src/test/java/com/solidbeans/call4help/Call4HelpApplicationTests.java
@@ -1,13 +1,25 @@
 package com.solidbeans.call4help;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class Call4HelpApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void verify_java_version_support() {
+		var textBlock = """
+				This is a test
+				to see if current environment
+				support some new features 
+				in Java 15""";
+		int actualNrOfRows = (int)textBlock.lines().count();
+		int expectNrOfRows = 4;
+		Assertions.assertEquals(expectNrOfRows, actualNrOfRows);
 	}
 
 }
