c09f42fc34c77e3f6e8246d343ad12e94d275dde
Halimove01 <halim.dakir@gmail.com>
2021-01-26 12:42:37 +0100

    Added User repository

---
+++ src/main/java/com/solidbeans/call4help/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.solidbeans.call4help.repository;
+
+import com.solidbeans.call4help.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
