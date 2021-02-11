10eb18839696ba70029ea8d3ecd7d3b00c7d3e7f
Halimove01 <halim.dakir@gmail.com>
2021-02-11 10:18:06 +0100

    added userid to not get whos need help in the rearest list

--- src/main/java/com/solidbeans/call4help/repository/PositionRepository.java
+++ src/main/java/com/solidbeans/call4help/repository/PositionRepository.java
@@ -1,13 +1,10 @@
 package com.solidbeans.call4help.repository;
 
-import com.solidbeans.call4help.dto.DistanceDTO;
 import com.solidbeans.call4help.entity.Position;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +15,8 @@ public interface PositionRepository extends CrudRepository<Position, Long>, Pagi
     @Query("SELECT DISTINCT p FROM Position p INNER JOIN FETCH p.users u WHERE u.userId=:userId")
     Optional<Position> findPositionByUserId(String userId);
 
-    List<Position> findAllByMunicipality(String city);
+    @Query("SELECT DISTINCT p FROM Position p INNER JOIN FETCH p.users u WHERE p.municipality=:city AND u.userId<>:userId")
+    List<Position> findAllByCityAndUserId(String city, String userId);
 
     @Query("SELECT DISTINCT p FROM Position p")
     List<Position> findAllPosition();
