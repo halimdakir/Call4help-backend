8c1e018b6c18ed3f14249a09b58240b97f52bd00
Halimove01 <halim.dakir@gmail.com>
2021-02-11 15:52:32 +0100

    fixed build error

--- src/main/java/com/solidbeans/call4help/repository/ReportRepository.java
+++ src/main/java/com/solidbeans/call4help/repository/ReportRepository.java
@@ -2,6 +2,7 @@ package com.solidbeans.call4help.repository;
 
 import com.solidbeans.call4help.entity.Report;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRepository extends CrudRepository<Report, Long> {
