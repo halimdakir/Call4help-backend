20b0d5ab4a652402317ed4167e86c1beb30b0a40
Adam Halim Dakir <halim.dakir@gmail.com>
2021-02-11 14:39:50 +0000

    ReportRepository.java edited online with Bitbucket

--- src/main/java/com/solidbeans/call4help/repository/ReportRepository.java
+++ src/main/java/com/solidbeans/call4help/repository/ReportRepository.java
@@ -3,5 +3,6 @@ package com.solidbeans.call4help.repository;
 import com.solidbeans.call4help.entity.Report;
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface ReportRepository extends CrudRepository<Report, Long> {
 }
