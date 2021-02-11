a868a31dc79c4eb9482a97b7d451bbec01bf86c2
Halimove01 <halim.dakir@gmail.com>
2021-02-11 13:05:30 +0100

    Rest that return list of cities

--- src/main/java/com/solidbeans/call4help/controller/CitiesController.java
+++ src/main/java/com/solidbeans/call4help/controller/CitiesController.java
@@ -1,2 +1,23 @@
-package com.solidbeans.call4help.controller;public class CitiesController {
+package com.solidbeans.call4help.controller;
+
+import com.solidbeans.call4help.file.Location;
+import com.solidbeans.call4help.service.CitiesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/cities")
+public class CitiesController {
+
+    @Autowired
+    private CitiesService citiesService;
+
+    @GetMapping("/all")
+    public List<Location> getAllCities(){
+     return citiesService.allCities();
+    }
 }
