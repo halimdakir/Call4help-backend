b8516128af097137553e37782614b27b03d7e208
Tyson La <tyson@tcmt.se>
2021-02-23 15:20:30 +0100

    reformatted reportcontroller

--- src/main/java/com/solidbeans/call4help/controller/ReportController.java
+++ src/main/java/com/solidbeans/call4help/controller/ReportController.java
@@ -1,9 +1,8 @@
 package com.solidbeans.call4help.controller;
 
-import com.solidbeans.call4help.dto.ReportDTO;
 import com.solidbeans.call4help.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,15 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/report")
+@RequestMapping(value = "/api/v1/report", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ReportController {
 
     @Autowired
     private ReportService reportService;
 
     @PostMapping
-    public ResponseEntity<ReportDTO> registerReport(Long userId, @RequestBody String text){
-        var report = reportService.saveReport(userId, text);
-        return new ResponseEntity<>(report, HttpStatus.OK);
+    public ResponseEntity<?> registerReport(Long userId, @RequestBody String text) {
+        return ResponseEntity.ok(reportService.saveReport(userId, text));
     }
 }
